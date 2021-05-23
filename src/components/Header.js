e6694e1f2bb14e0eaa10b9997e8707a283930383
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-05-23 16:30:36 +0100

    added files

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 function Header() {
   return (
     <header>
-      <div>
-        <div>
+      <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
+            objectFit="contain"
+            className="cursor-pointer"
           />
         </div>
       </div>
