e4f8f328ed60eb8a462e89953c7071f236c33d21
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-05-27 16:54:46 +0100

    edited files

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 function Banner() {
-  return <div className="relative"></div>;
+  return (
+    <div className="relative">
+      <Carousel
+        autoPlay
+        infiniteLoop
+        showStatus={false}
+        showIndicators={false}
+        showThumbs={false}
+        interval={5000}
+      >
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/gil" alt="" />
+        </div>
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
+        </div>
+        <div>
+          <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
+        </div>
+      </Carousel>
+    </div>
+  );
 }
 
 export default Banner;
