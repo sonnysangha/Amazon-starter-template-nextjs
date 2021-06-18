516d29c9148af369daf7d734061ea4b57bd8587d
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2021-06-18 15:19:50 +0100

    edited files

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import { useState } from "react";
+import { StarIcon } from "@heroicons/react/solid";
 
 function Product({ id, title, price, description, category, image }) {
-  return <div></div>;
+  const [rating] = useState(2);
+  return (
+    <div>
+      <p>{category}</p>
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      <h4>{title}</h4>
+      <div>
+        <StarIcon className="h-5" />
+      </div>
+    </div>
+  );
 }
 
 export default Product;
