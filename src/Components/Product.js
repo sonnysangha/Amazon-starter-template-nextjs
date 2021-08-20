17fc6a43c7db58d04c715ed2aeba4ea610215d33
chestno.02 <atuly152001@gmail.com>
2021-08-20 21:34:45 +0530

    Product feed completed

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,32 +1,40 @@
-import { Currency, CurrencyRupeeIcon } from '@heroicons/react/outline';
+import Currency from "react-currency-formatter"
 import { StarIcon } from '@heroicons/react/solid'
 import Image from 'next/image'
 import React, { useState } from 'react'
-const Max_Rating=5;
-const Min_Rating=5;
-function Product({title,id,category,image,price,description}) {
-  
-     const [rating] = useState( Math.floor(Math.random()*(Max_Rating-Min_Rating+1) +Min_Rating))
-     const [hasPrime] = useState(Math.random()<0.5)
-     return (
-        <div>
-            <p>{category}</p>
-             <Image src={image} height={200} width={200} objectFit="contain" />
 
-             <h4>{title}</h4>
-             <div className=" flex" >{
-                  Array(rating).
-                  fill().
-                  map((_,i) => (      
-                  <StarIcon className="h-5 "  />
-                  ))}
-              <p>{description}</p>
-             <div className="  " >
-                
-                 <Currency quantity={price}  />
-             </div>
+function Product({ title, id, category, image, price, description }) {
+
+    const [rating] = useState(Math.floor(Math.random()*5)+1)
+    const [hasPrime] = useState(Math.random() < 0.5)
+    console.log(Math.random());
+    return (
+        <div className=" relative flex flex-col bg-white m-5 z-30 p-10 " >
+            <p className=" absolute top-2 right-2 text-xs italic text-gray-400 " >{category}</p>
+            <Image src={image} height={200} width={200} objectFit="contain" />
+
+            <h4 className=" my-3 " >{title}</h4>
+            <div className=" flex" >{
+                Array(rating).
+                    fill().
+                    map((_, i) => (
+                        <StarIcon className="h-5 text-yellow-500 " />
+                    ))}
+                    </div>
+                <p className=" text-xs mt-2 mb-2 line-clamp-2 " >{description}</p>
+                <div className=" m-5  " >
+
+                    <Currency quantity={price} currency="INR" />
                 </div>
-        </div>
+                {hasPrime && (
+                    <div className=" flex items-center space-x-2 -mt-5 " >
+                        <img className=" w-12 " src="https://links.papareact.com/fdw" alt="" />
+                         <p className=" text-xs text-gray-500 " >Free Next-day Delivery</p>
+                    </div>
+                )}
+                   <button className=" mt-auto button " > Add to Cart </button>
+            </div>
+        
     )
 }
 
--- src/Components/ProductFeed.js
+++ src/Components/ProductFeed.js
@@ -3,8 +3,8 @@ import Product from './Product'
 
 function ProductFeed({products}) {
     return (
-        <div>
-            {products.map(({title,id,category,image,price,description})=>(
+        <div className="  grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52 mx-auto" >
+            {products.slice(0,4).map(({title,id,category,image,price,description})=>(
                 
               <Product 
               key={id}
@@ -15,7 +15,32 @@ function ProductFeed({products}) {
               description={description}
               />
              ) )}
-        </div>
+              <img className="md:col-span-full" src="https:links.papareact.com/dyz" alt="" />
+          <div className=" md:col-span-2" >
+          {products.slice(4,5).map(({title,id,category,image,price,description})=>(
+                
+                <Product 
+                key={id}
+                title={title}
+                category={category}
+                image={image}
+                price={price}
+                description={description}
+                />
+               ) )}
+          </div>
+          {products.slice(5,products.length).map(({title,id,category,image,price,description})=>(
+                
+                <Product 
+                key={id}
+                title={title}
+                category={category}
+                image={image}
+                price={price}
+                description={description}
+                />
+               ) )}
+   </div>
     )
 }
 
