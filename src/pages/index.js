1dacb602f10716799528e18954cf3694d860e0a4
Seyma Ciftci <seyma.ciftci@elision.eu>
2022-11-14 10:36:29 +0100

    Built banner component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,19 @@
 import Head from "next/head";
+import Banner from "../components/Banner";
 import Header from "../components/Header";
 
 export default function Home() {
 	return (
-		<div>
+		<div className='bg-gray-100 '>
 			<Head>
 				<title>Amazon 2.0</title>
 			</Head>
 
 			<Header />
+
+			<main className='mx-auto max-w-screen-2xl'>
+				<Banner />
+			</main>
 		</div>
 	);
 }
